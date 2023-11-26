1f9b63af0f63a6be8a898a2347e1fdfe25ac32d1
alelikak <141032811+alelikak@users.noreply.github.com>
2023-11-26 13:19:54 -0500

    Add files via upload

--- utils/list_helper.cjs
+++ utils/list_helper.cjs
@@ -2,7 +2,34 @@ const dummy = (blogs) => {
   var a=1
   return a
 }
+const totalLikes = (blogs) => {
+
+  const sum = blogs.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.likes,
+    0,//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+  )
+  return sum
+}
+
+
+const favoriteBlog = (blogs) => {
+
+  var suurinindeksi = blogs.reduce((iMax, x, i, arr) => x.likes > arr[iMax].likes ? i : iMax, 0)
+  //https://stackoverflow.com/questions/11301438/return-index-of-greatest-value-in-an-array
+
+  console.log(blogs[suurinindeksi])
+
+  var k = JSON.parse(JSON.stringify( blogs[suurinindeksi], ['title','likes','author'] , 4))
+  console.log(k)
+  //https://stackoverflow.com/questions/16542529/how-to-change-the-order-of-the-fields-in-json
+
+  return  k
+}
+
+
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
